25febe2d34270f72a24138d293c67ea21920d5b8
bishalsth <bishal.sth.3@gmail.com>
2022-11-08 22:25:31 +0545

    User Sign up

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,11 +1,24 @@
 package com.smart.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@RequestMapping("/")
 	public String home(Model model) {
@@ -22,7 +35,40 @@ public class HomeController {
 	@RequestMapping("/signup")
 	public String signup(Model model) {
 		model.addAttribute("titile","Register-Smart Contact Manager");
+		model.addAttribute("user",new User());
 		return "signup";
+	}
+	
+//	Handler for Registering user
+	@PostMapping("/do_register")
+	public String registerUser(@ModelAttribute("user") User user,@RequestParam(value="agreement",defaultValue = "false") boolean agreement,Model model,HttpSession session) {
+		
+	try {
+		if(!agreement) {
+			System.out.println("You have not agreed the terms and conditions");
+			throw new Exception("You have not agreed the terms and conditions");
+			
+		}
+		user.setRole("Role_user");
+		user.setEnabled(true);
+		user.setImageUrl("default.png");
+		
+		System.out.println("Agreement"+ agreement);
+		System.out.println("USer"+ user);
+		User result = this.userRepository.save(user);
+		
+		model.addAttribute("user",new User());
+		session.setAttribute("message", new Message("Successfully Registerd","alert-success"));
+		return "signup";
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		model.addAttribute("user",user);
+		session.setAttribute("message", new Message("Something Went Wrong!! "+e.getMessage(),"alert-danger"));
+		return "signup";
+		
+	}
+		
 	}
 
 }
